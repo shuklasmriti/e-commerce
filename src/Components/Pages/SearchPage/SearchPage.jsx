73f6a7b1df24f32a832c4ee0c5f8755be8735017
smriti <shuklasmriti83@gmail.com>
2024-06-25 23:57:16 +0530

    handled key press

--- src/Components/Pages/SearchPage/SearchPage.jsx
+++ src/Components/Pages/SearchPage/SearchPage.jsx
@@ -1,45 +1,44 @@
 import React from 'react';
 import { useState, useEffect } from "react";
 import { useSearchParams } from 'react-router-dom';
-
+import './SearchPage.css'
 import Cards from '../../Cards/Cards';
-<link rel="stylesheet" href="./Components/Pages/SearchPage/SearchPage.css" />
 
 const SearchPage = () => {
   
     const [queryParameters] = useSearchParams()
-    const [search, setSearch] = useState(queryParameters.get('search'));
+    const search =queryParameters.get('search');
   
     const [searchResults, setSearchResults] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    // const [inputValue, setInputValue] = useState("");
     console.log(search);
 
     
-      const handleKeyPress = (event) => {
-        setIsLoading(true);
-          fetch(`https://dummyjson.com/products/search?q=${search}`)
-            .then((res) => res.json())
-            .then((data) => {
-              setSearchResults(data?.products);
-              setIsLoading(false);
-            })
-            .catch((error) => {
-              console.error("Error fetching search results:", error);
-            })
-            .finally(()=>{
-                setIsLoading(false);
-            })
-            ;
-            
-        
-      };
+    
 
 console.log(searchResults);
       
  useEffect(()=>{
+  const handleKeyPress = (event) => {
+    setIsLoading(true);
+      fetch(`https://dummyjson.com/products/search?q=${search}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setSearchResults(data?.products);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error fetching search results:", error);
+        })
+        .finally(()=>{
+            setIsLoading(false);
+        });
+  };
+
+
+
     handleKeyPress();
- },[])
+ },[search])
   
    
  return( 
