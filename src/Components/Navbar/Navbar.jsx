b0a7fb64576f856192715bef6afff4fa9dc82e4b
smriti <shuklasmriti83@gmail.com>
2024-07-06 18:13:40 +0530

    hamburger

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,43 +1,32 @@
-import React, { useContext } from "react";
-import { useState } from "react";
+import React, { useContext, useState } from "react";
 import FusionFindsLogo from "../Assets/logo2.png";
 import Badge from "@mui/material/Badge";
-import { FaUser } from "react-icons/fa";
+import { FaUser, FaShoppingCart, FaHeart, FaHome } from "react-icons/fa";
 import { FiLogOut } from "react-icons/fi";
-import { FaShoppingCart } from "react-icons/fa";
-import { FaHeart } from "react-icons/fa";
-import { FaHome } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { CartContext } from "../../context/CartContext";
 import { WishlistContext } from "../../context/WishlistContext";
 import { toast } from "react-toastify";
 import useSearch from "../../Hook/useSearch";
-<link rel="stylesheet" href="./src/Components/Navbar/Navbar.css" />;
+import { RxHamburgerMenu } from "react-icons/rx";
+
+import "./Navbar.css";
 
 export const Navbar = () => {
-  // const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => setIsOpen(!isOpen);
 
-  // const toggleMenu = () => {
-  //   setIsOpen(!isOpen);
-  // };
- const {  searchInput,searchChangeHandeler }=useSearch()
-
-
+  const { searchInput, searchChangeHandeler } = useSearch();
   const { cart } = useContext(CartContext);
   const { wishlist } = useContext(WishlistContext);
   const [userData, setUserData] = useState(
     JSON.parse(sessionStorage.getItem("userData"))
   );
-  // const history = useNavigate();
-
-const [dropdown,setDropdown]=useState(false);
-
-  
+  const [dropdown, setDropdown] = useState(false);
 
   const logoutButtonHandler = () => {
     sessionStorage.removeItem("userData");
     setUserData("");
-    // history.push("/");
     toast.success("Logout Successfully");
     setDropdown(false);
   };
@@ -49,36 +38,34 @@ const [dropdown,setDropdown]=useState(false);
           <img src={FusionFindsLogo} alt="" />
         </Link>
       </div>
-      <div className="navbar-menu" >
+      <div className="navbar-menu">
         <div className="search">
-             <form role="search" method="get" action="/searchpage">
-               <div className="input-group">
-              <input value={searchInput} type="text" placeholder="Search your product" className="form-control" name="search" onChange={searchChangeHandeler} />
-              </div>
-              </form>
+          <form role="search" method="get" action="/searchpage">
+            <div className="input-group">
+              <input
+                value={searchInput}
+                type="text"
+                placeholder="Search your product"
+                className="form-control"
+                name="search"
+                onChange={searchChangeHandeler}
+              />
             </div>
+          </form>
+        </div>
 
         <div className="icons">
-          {/* <CiSearch size={23} className="icon1" /> */}
           <Link className="link" to="/">
             <FaHome size={23} className="icon2" />
           </Link>
           <Link className="link" to="/cart">
-            <Badge
-              badgeContent={cart?.length ?? 0}
-              className="icon4"
-              color="primary"
-            >
+            <Badge badgeContent={cart?.length ?? 0} className="icon4" color="primary">
               <FaShoppingCart size={23} className="icon4" />
             </Badge>
           </Link>
 
           <Link className="link" to="/wishlist">
-            <Badge
-              badgeContent={wishlist?.length ?? 0}
-              className="icon5"
-              color="primary"
-            >
+            <Badge badgeContent={wishlist?.length ?? 0} className="icon5" color="primary">
               <FaHeart size={23} className="icon5" />
             </Badge>
           </Link>
@@ -90,41 +77,56 @@ const [dropdown,setDropdown]=useState(false);
                 to="#"
                 id="navbarDropdown"
                 role="button"
-                data-bs-toggle="dropdown"
                 aria-expanded="false"
-                onClick={()=>setDropdown(!dropdown)}
+                onClick={() => setDropdown(!dropdown)}
               >
-                <i>
-                  <FaUser />
-                </i>{" "}
-                {userData.username}
+                <FaUser /> {userData.username}
               </Link>
             ) : (
               <Link to="/login" className="nav-link" id="login" role="button">
-                {" "}
-                <i>
-                  <FaUser />
-                </i>
-                Login
+                <FaUser /> Login
               </Link>
             )}
-                
-              <div className="dropdown-menu" aria-labelledby="navbarDropdown" style={{display:dropdown?"block":"none"}}>
-                <div>
-                  <Link
-                    to="#"
-                    className="dropdown-item" 
-                    onClick={logoutButtonHandler}
-                  >
-                    <i>
-                      <FiLogOut />
-                    </i>{" "}
-                    Logout
-                  </Link>
-                </div>
+            {userData && (
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown" style={{ display: dropdown ? "block" : "none" }}>
+                <Link to="#" className="dropdown-item" onClick={logoutButtonHandler}>
+                  <FiLogOut /> Logout
+                </Link>
               </div>
-               
+            )}
           </div>
+        </div>
+      </div>
+      <div className="hamburger"  onClick={toggleMenu}>
+      <RxHamburgerMenu style={{ width:"20px" , height:"37px"}}  />
+        {/* <div className="bar"></div> */}
+        {/* <div className="bar"></div> */}
+        {/* <div className="bar"></div>  */}
+      </div>
+      <div className={`mobile-menu ${isOpen ? 'open' : ''}`}>
+        <Link to="/" onClick={toggleMenu}>Home</Link>
+        <Link to="/cart" onClick={toggleMenu}>Cart</Link>
+        <Link to="/wishlist" onClick={toggleMenu}>Wishlist</Link>
+        
+        {userData ? (
+          <Link to="#" onClick={() => { logoutButtonHandler(); toggleMenu(); }}>Logout</Link>
+        ) : (
+          <Link to="/login" onClick={toggleMenu}>Login</Link>
+        )}
+
+<div className="search">
+          <form role="search" method="get" action="/searchpage">
+            <div className="input-group">
+              <input
+                value={searchInput}
+                type="text"
+                placeholder="Search your product"
+                className="form-control"
+                name="search"
+                onChange={searchChangeHandeler}
+              />
+            </div>
+          </form>
         </div>
       </div>
     </div>
